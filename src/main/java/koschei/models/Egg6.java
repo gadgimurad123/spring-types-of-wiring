c30be67f9361540be8a2bb93e8df25d97b0049f0
Gadzhimurad <gadgimurad123@gmail.com>
2020-12-23 02:52:34 +0300

    Task is done

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Egg6 {
+    @Autowired
+    private Needle7 needle7;
+
+    public void setNeedle7(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle7.toString();
     }
 }
